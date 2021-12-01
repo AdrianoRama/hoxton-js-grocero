30968ce910e82cdb29b943ed9d92e6ab49efe848
Adriano Rama <ramaadriano@outlook.com>
2021-12-02 00:56:48 +0100

    Fixed the images

--- index.js
+++ index.js
@@ -10,109 +10,130 @@ This is how an item object should look like
 
 */
 
-const state = [ 
-  groceries = {
-    id: '001',
+const state = {
+  groceries : [ {
+    id: 1,
     name: 'beetroot',
     price: 0.35,
     amount: 0
   },
   {
-    id: '002',
+    id: 2,
     name: 'carrot',
-    price: 0.35,
+    price: 0.30,
     amount: 0
   },
   {
-    id: '003',
+    id: 3,
     name: 'apple',
-    price: 0.35,
+    price: 0.25,
     amount: 0
   },
   {
-    id: '004',
+    id: 4,
     name: 'apricot',
-    price: 0.35,
+    price: 0.65,
     amount: 0
   },
   {
-    id: '005',
+    id: 5,
     name: 'avocado',
-    price: 0.35,
+    price: 0.45,
     amount: 0
   },
   {
-    id: '006',
+    id: 6,
     name: 'bananas',
     price: 0.35,
     amount: 0
   },
   {
-    id: '007',
+    id: 7,
     name: 'bell-pepper',
-    price: 0.35,
+    price: 0.15,
     amount: 0
   },
   {
-    id: '008',
+    id: 8,
     name: 'berry',
-    price: 0.35,
+    price: 0.385,
     amount: 0
   },
   {
-    id: '009',
+    id: 9,
     name: 'blueberry',
-    price: 0.35,
+    price: 0.95,
     amount: 0
   },
   {
-    id: '010',
+    id: 10,
     name: 'eggplant',
-    price: 0.35,
+    price: 0.25,
     amount: 0
   }
-  
-]
+  ]}
 
 
-function storeItem() {
+function storeItems() {
+  const storeUl = document.querySelector(".item-list.store--item-list")
+
+  for (const grocery of state.groceries){
+
   const liEl = document.createElement(`li`)
+
   const storeItemIcon = document.createElement(`div`)
   storeItemIcon.setAttribute(`class`, `store--item-icon`)
+
   const btnEl = document.createElement(`button`)
   btnEl.textContent = `Add to cart`
+
   const imgEl = document.createElement(`img`)
-  imgEl.setAttribute(`src`, `assets/icons/001-beetroot.svg`)
+  imgEl.setAttribute(`src`, `assets/icons/${grocery.id<10?'00':'0'}${grocery.id}-${grocery.name}.svg`)
   imgEl.setAttribute(`alt`, `beetroot`)
+
+  storeUl.append(liEl)
   liEl.append(storeItemIcon, btnEl)
   storeItemIcon.append(imgEl)
+  }
 }
+storeItems()
 
-function cartItem() {
+function cartItems() {
+
+  const cartUl = document.querySelecton("#cart .item-list.cart--item-list")
+
   const liEl = document.createElement(`li`)
+
   const cartItemIcon = document.createElement(`img`)
   cartItemIcon.setAttribute(`class`, `cart--item-icon`)
   cartItemIcon.setAttribute(`src`, `assets/icons/001-beetroot.svg`)
   cartItemIcon.setAttribute(`alt`, `beetroot`)
+
   const paragraphEl = document.createElement(`p`)
   paragraphEl.textContent = `beetroot`
+
   const quantityBtn = document.createElement(`button`)
   quantityBtn.setAttribute(`class`, `quantity-btn remove-btn center`)
   quantityBtn.textContent =  `-`
+
   const quantityTxt = document.createElement(`span`)
   quantityTxt.setAttribute(`class`, `quantity-text center`)
   quantityTxt.textContent = `1`
+
   const addBtn = document.createElement(`button`)
   addBtn.setAttribute(`class`, `quantity-btn add-btn center`)
   addBtn.textContent = `+`
+
+  cartUl.append(liEl)
   liEl.append(cartItemIcon, paragraphEl, quantityBtn, quantityTxt, addBtn)
 
 }
-
-
+cartItems()
 
 
 
 function render() {
 
 }
+
+render()

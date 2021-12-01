69380ca125c273ea88237bef93876ec4fa2ab2f4
Adriano Rama <ramaadriano@outlook.com>
2021-12-01 21:22:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+/*
+
+This is how an item object should look like
+
+{
+  id: 1, // <- the item id matches the icon name in the assets/icons folder
+  name: "beetroot",
+  price: 0.35 // <- You can come up with your own prices
+}
+
+*/
+
+const state = [ 
+  groceries = {
+    id: '001',
+    name: 'beetroot',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '002',
+    name: 'carrot',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '003',
+    name: 'apple',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '004',
+    name: 'apricot',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '005',
+    name: 'avocado',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '006',
+    name: 'bananas',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '007',
+    name: 'bell-pepper',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '008',
+    name: 'berry',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '009',
+    name: 'blueberry',
+    price: 0.35,
+    amount: 0
+  },
+  {
+    id: '010',
+    name: 'eggplant',
+    price: 0.35,
+    amount: 0
+  }
+  
+]
+
+
+
+function render() {
+
+}
